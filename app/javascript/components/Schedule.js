1de5aceb848cfe1bf0f880256d471e18987c00da
Cássio Maletich <cgm1010@gmail.com>
2021-01-16 20:32:28 -0300

    begin calendar

--- app/javascript/components/Schedule.js
+++ app/javascript/components/Schedule.js
@@ -1,14 +1,38 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
+
+const localizer = momentLocalizer(moment)
+
 class Schedule extends React.Component {
-  componentDidMount() {
-    console.log("Init", this.props.appointments);
+  state = {
+    appointments: []
   }
+
+  componentDidMount() {
+    const appointments = this.props.appointments
+    appointments.map((a) => {
+      a.start = moment(a.date).toDate();
+      a.end = moment(a.date).add(30, "minutes").toDate();
+    })
+    this.setState({ appointments })
+  }
+
   render () {
     return (
       <React.Fragment>
         <div>
-          <h2>Schedule test</h2>
+          <h2>Agenda da Sala</h2>
+
+          <Calendar
+            localizer={localizer}
+            events={this.state.appointments}
+            startAccessor="start"
+            endAccessor="end"
+            titleAccessor="name"
+            style={{ height: 500 }}
+          />
         </div>
       </React.Fragment>
     );
