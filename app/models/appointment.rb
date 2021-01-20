7466a3e7feab928f29aea9cc854294228a49a9ab
Cássio Maletich <cgm1010@gmail.com>
2021-01-20 00:49:22 -0300

    validate week days

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,11 +1,18 @@
 class Appointment < ApplicationRecord
   belongs_to :room
   belongs_to :user
-  validate :check_time_window, :check_conflict
+  validate :check_time_window, :check_day_window, :check_conflict
 
   def check_time_window
     unless self.start.hour >= 8 && self.start.hour < 18 && self.end.hour > 8 && self.end.hour <= 18
       errors.add(:date, "Fora do horário permitido")
+    end
+  end
+
+  def check_day_window
+    excluded_days = [0, 6]
+    if excluded_days.any? { |ed| ed == self.start.wday || ed == self.end.wday }
+      errors.add(:date, "Fora dos dias permitidos")
     end
   end
 
