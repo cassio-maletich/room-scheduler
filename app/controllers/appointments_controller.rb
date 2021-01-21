01e98f30ac759eeba0bcce057a31aaab5c9c7d4a
Cássio Maletich <cgm1010@gmail.com>
2021-01-21 18:20:51 -0300

    redirect to schedule

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -36,7 +36,7 @@ class AppointmentsController < ApplicationController
     
     respond_to do |format|
       if @appointment.save
-        format.html { redirect_to @appointment, notice: 'Appointment was successfully created.' }
+        format.html { redirect_to appointments_path(room: @appointment.room.id), notice: 'Appointment was successfully created.' }
         format.json { render :show, status: :created, location: @appointment }
       else
         format.html { render :new }
